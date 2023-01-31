70631eb4265398e29e986b5745f24d47b56b9822
ajlvi <mtgeloproject@gmail.com>
2023-01-31 11:56:53 -0500

    Python now updates database stats

--- src/app/pick-history/pick-history.component.ts
+++ src/app/pick-history/pick-history.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../auth/auth.service';
+import { FirestoreService } from '../shared/firestore.service';
+import { RecordData } from '../shared/game.model';
 
 @Component({
   selector: 'app-pick-history',
   templateUrl: './pick-history.component.html',
   styleUrls: ['./pick-history.component.css']
 })
-export class PickHistoryComponent {
+export class PickHistoryComponent implements OnInit {
+  constructor(private fire: FirestoreService) {}
+  recordData: RecordData;
+  
+  ngOnInit(): void {
+    this.fire.getUserStats().subscribe(
+      (response: RecordData) => { this.recordData = response; }
+    )
+  }
 }
