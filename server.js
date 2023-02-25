4a7e1bab2655fbd591d769d3c5ddf7898d3ffd8a
ajlvi <mtgeloproject@gmail.com>
2023-02-24 23:09:49 -0500

    trying to make index.html actually render

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 app.use(express.static(__dirname + '/dist/nba-tracker-15'));
 
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/nba-tracker-15/index.html'));
+app.use('/', function(req,res) {
+    res.sendFile(path.join(__dirname, 'dist', 'nba-tracker-15', 'index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
