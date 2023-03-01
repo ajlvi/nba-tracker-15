27117843819194e1052381a763197e10cf3877b5
ajlvi <mtgeloproject@gmail.com>
2023-03-01 00:02:38 -0500

    Group page now can display rosters

--- src/app/account/groups-update/group-config/group-config.component.ts
+++ src/app/account/groups-update/group-config/group-config.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core'
+import { SeenDataService } from 'src/app/shared/seen-data-service';
+import { UserData } from 'src/app/shared/user-data.interface';
 
 @Component({
   selector: 'app-group-config',
@@ -10,5 +12,34 @@ export class GroupConfigComponent {
   @Input() groupname: string;
   @Output() deleted = new EventEmitter<null>()
 
+  roster: {[user: string]: UserData} = {};
+  expanded: boolean = false;
+
+  constructor (private seen: SeenDataService) {}
+
   onDelete() { this.deleted.emit() }
+
+  onExpand() {
+    this.expanded = true;
+    if (!this.seen.sawGroup(this.groupname)) {
+      this.seen.getGroupRoster(this.groupname).subscribe(
+        () => {
+          for (let user of this.seen.GroupRosters[this.groupname]) {
+            this.roster[user] = this.seen.UserData[user];
+          };
+          console.log(this.roster);
+        }
+      )
+    }
+    else { 
+      for (let user of this.seen.GroupRosters[this.groupname]) {
+        this.roster[user] = this.seen.UserData[user];
+      }; 
+    }
+  }
+
+  onContract() {
+    this.expanded = false;
+  }
+
 }
